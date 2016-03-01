8587bbd890eaaa974a03c922d46de32d1c224117
bhare1987 <brandonlhare@gmail.com>
2016-02-29 21:37:21 -0500

    finished

--- js/popup.js
+++ js/popup.js
@@ -4,7 +4,9 @@ var ghqv = {
     ghqv.events();
   },
   presentation: function(){
-    ghqv.checkUserName();
+    ghqv.getUserData();
+    ghqv.getRepoData();
+    setTimeout(ghqv.checkUserName, 500);
   },
   config: {
     urlBase: "https://api.github.com/users",
@@ -23,40 +25,51 @@ var ghqv = {
       ghqv.getUserData();
       ghqv.getRepoData();
       $input.val("").addClass('hide');
-      $div.removeClass('hide').html(ghqv.compileTemplate(handlebarTemplates.user(), ghqv.config.userData));
-      ghqv.addReposToDom(ghqv.config.reposData, '.reposContainer');
+      $div.removeClass('hide');
+      ghqv.buildUserHTML(ghqv.config.userData, '.prompt div');
+      ghqv.buildRepoHTML('.reposContainer');
     }
   },
   checkUserName: function(){
     if (localStorage.getItem('ghqvUserName')) {
       var $input = $('input[name="username"]');
       var $div = $('.prompt div');
-      ghqv.getUserData();
-      ghqv.getRepoData();
       $input.addClass('hide');
-      $div.removeClass('hide').html(ghqv.compileTemplate(handlebarTemplates.user(), ghqv.config.userData));
-      ghqv.addReposToDom(ghqv.config.reposData, '.reposContainer');
+      $div.removeClass('hide');
+      ghqv.buildUserHTML(ghqv.config.userData, '.prompt div');
+      ghqv.buildRepoHTML('.reposContainer');
     }
   },
-  // getTmpl: function(templateName){
-  //   return templates[templateName];
-  // },
-  // constructTmpl: function(templateName){
-  //   var tmpl = ghqv.getTmpl(templateName);
-  //   var tmplFunc = _.template(tmpl);
-  //   return tmplFunc;
-  // },
-  // constructContent: function(templateName, object){
-  //   var output = ghqv.constructTmpl(templateName);
-  //   return output(object);
-  // },
-  addReposToDom: function(repos, container) {
-    ghqv.compileTemplate(handlebarTemplates.repos, repos);
-    $(container).append(ghqv.compileTemplate(handlebarTemplates.repos(), repos));
+  buildUserHTML: function(data, container){
+    userStr = "<div class='userInfo'><img src='"
+            + ghqv.config.userData.avatar_url
+            + "'><ul><li class='username'><strong>Username:</strong> "
+            + ghqv.config.userData.login
+            + "</li><li class='location'><strong>Location:</strong> "
+            + ghqv.config.userData.location
+            + "</li><li class='repototal'><strong>Number of Repos:</strong> "
+            + ghqv.config.userData.public_repos
+            + "</li></ul></div>";
+    console.log(userStr);
+    $(container).html(userStr);
   },
-  compileTemplate: function(template, data){
-    var template = Handlebars.compile(template);
-    return template(data);
+  buildRepoHTML: function(container){
+    repoStr = "";
+    ghqv.config.reposData.forEach(function(el){
+      repoStr += "<div class='repoItem'><a href='"
+              +  el.repoUrl
+              +  "' target='_blank'>"
+              +  el.repoName
+              +  "</a>"
+              +  "<span class='repoUpdated'><strong>Last Updated:</strong> "
+              +  el.repoUpdated
+              +  "</span><span class='repoSSH'><strong>SSH:</strong> "
+              +  el.repoSSH
+              +  "</span><span class='repoClone'><strong>Clone:</strong> "
+              +  el.repoClone
+              +  "</span></div>"
+    });
+    $(container).html(repoStr);
   },
   getUserData: function() {
     var response = $.ajax({
@@ -86,13 +99,6 @@ var ghqv = {
       });
     });
   }
-
-
-
-
-
-
-
 
 }
 
